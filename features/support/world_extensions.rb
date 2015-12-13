331fd39ca06969ad627f4e85abe6cd650a8883d1
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-12-13 13:50:43 +0000

    cleaned up the cucumber steps and userinterface

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -1,6 +1,22 @@
 module KnowsTheUserInterface
 
 
+
+	class UserInterface
+		
+		include Capybara::DSL
+		
+		def withdraw_from(account, amount)
+			
+		    visit '/'
+			fill_in 'Account', :with => account.number
+			fill_in 'Amount', :with => amount
+			click_button 'Withdraw'	
+		end
+
+	end
+
+
 		def cash_slot
 		
 			@cash_slot ||= Cashslot.new(serial_number: 1)
@@ -14,6 +30,13 @@ module KnowsTheUserInterface
 			@my_account ||= Account.create(:number => "test", :balance =>0)
 	
 		end
+
+		def teller
+			@teller ||= UserInterface.new
+			
+		end
+
+
 
 	
 	
